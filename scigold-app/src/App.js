eafdb741f5fafe8c10204c81fa14b0fffa448da0
Katie Dotson <katherine.dotson10@gmail.com>
2018-09-08 15:52:28 -0400

    comment#

--- scigold-app/src/App.js
+++ scigold-app/src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class App extends Component {
+
+  onClick = (event) =>{
+    event.preventDefault;
+    <Clouds />
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Alexandria</h1>
-        </header>
+      <div className="App text-center">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">Alexandria</a>
+            </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+            <NavItem eventKey={1} href="#">
+              Discussion
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Workspace
+            </NavItem>
+            <NavItem eventKey={3} href="#">
+              Archives
+            </NavItem>
+            <NavItem eventKey={4} href="#">
+              Profile
+            </NavItem>
+          </Nav>
+        </Navbar>;
+        
+        <form className="form-signin">
+          <input type="text" id="inputEmail" class="form-control" placeholder="Search..." autofocus></input>
 
-        <div className="inputs">
+          <div className="form-group">
+            <input type="file" className="form-control" id="exampleFormControlFile1"></input>
+          </div>
 
-          <label for='' className='input-label'>Search: </label>
-          <input type='text' className='text-input' placeholder='Search...'></input>
+          <button className="btn btn-lg btn-light btn-block mt-5" type="submit" onClick=''>Submit</button>
 
-          <br/>
-          <label for='' className='input-label'>Upload: </label>
-          <input type='file' className='file-input'></input>
-
-        </div>
-
+        </form>
       </div>
     );
   }
 }
+
 
 export default App;

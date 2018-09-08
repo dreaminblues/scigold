fd9bfd18f5e6921a687e5c45334f5de71d2617fd
Katie Dotson <katherine.dotson10@gmail.com>
2018-09-08 18:30:31 -0400

    blah

--- scigold-app/src/data.js
+++ scigold-app/src/data.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Clouds } from './clouds';
+import {Clouds} from './clouds';
 
 class App extends Component {
+
+  onClick = (event) =>{
+    event.preventDefault;
+    <Clouds />
+  }
+
 
   render() {
     return (
@@ -15,8 +19,8 @@ class App extends Component {
             <Navbar.Brand>
               <a href="#home">Alexandria</a>
             </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
+            </Navbar.Header>
+            <Nav>
             <NavItem eventKey={1} href="#">
               Discussion
             </NavItem>
@@ -31,21 +35,10 @@ class App extends Component {
             </NavItem>
           </Nav>
         </Navbar>;
+        
+        
 
-        <form className="form-signin">
-          <input type="text" id="inputEmail" class="form-control" placeholder="Search..." autofocus></input>
-
-          <div className="form-group">
-            <input type="file" className="form-control" id="exampleFormControlFile1"></input>
-          </div>
-
-          <LinkContainer to='/cloud'>
-            <button className="btn btn-lg btn-light btn-block mt-5" type="submit">Submit</button>
-          </LinkContainer>
-
-        </form>
       </div>
-
     );
   }
 }
